6645133b074d8e105122c1de5ee7aafb05a3fe8e
SteAlb89 <98278002+SteAlb89@users.noreply.github.com>
2023-10-05 14:33:13 +0200

    change desing from bootstrap with design from syncfusion

--- Expenses Tracker/Controllers/CategoryController.cs
+++ Expenses Tracker/Controllers/CategoryController.cs
@@ -65,6 +65,14 @@ namespace Expenses_Tracker.Controllers
         {
             if (ModelState.IsValid)
             {
+                if (category.CategoryId == 0)
+                {
+                    _context.Add(category);
+                }
+                else
+                {
+                    _context.Update(category);
+                }
                 _context.Add(category);
                 await _context.SaveChangesAsync();
                 return RedirectToAction(nameof(Index));
