03d7403c51479e159579874f34db976b47b86ea1
SteAlb89 <98278002+SteAlb89@users.noreply.github.com>
2023-10-05 17:24:43 +0200

    refactoring code and add delete button in action category

--- Expenses Tracker/Controllers/CategoryController.cs
+++ Expenses Tracker/Controllers/CategoryController.cs
@@ -26,24 +26,6 @@ namespace Expenses_Tracker.Controllers
                         Problem("Entity set 'ApplicationDbContext.Categories'  is null.");
         }
 
-        // GET: Category/Details/5
-        public async Task<IActionResult> Details(int? id)
-        {
-            if (id == null || _context.Categories == null)
-            {
-                return NotFound();
-            }
-
-            var category = await _context.Categories
-                .FirstOrDefaultAsync(m => m.CategoryId == id);
-            if (category == null)
-            {
-                return NotFound();
-            }
-
-            return View(category);
-        }
-
         // GET: Category/Create
         public IActionResult AddOrEdit(int id = 0)
         {
@@ -53,7 +35,6 @@ namespace Expenses_Tracker.Controllers
             }
             else
                 return View(_context.Categories.Find(id));
-
         }
 
         // POST: Category/Create
@@ -73,30 +54,9 @@ namespace Expenses_Tracker.Controllers
                 {
                     _context.Update(category);
                 }
-
                 await _context.SaveChangesAsync();
                 return RedirectToAction(nameof(Index));
             }
-            return View(category);
-        }
-
-
-
-        // GET: Category/Delete/5
-        public async Task<IActionResult> Delete(int? id)
-        {
-            if (id == null || _context.Categories == null)
-            {
-                return NotFound();
-            }
-
-            var category = await _context.Categories
-                .FirstOrDefaultAsync(m => m.CategoryId == id);
-            if (category == null)
-            {
-                return NotFound();
-            }
-
             return View(category);
         }
 
@@ -114,14 +74,8 @@ namespace Expenses_Tracker.Controllers
             {
                 _context.Categories.Remove(category);
             }
-
             await _context.SaveChangesAsync();
             return RedirectToAction(nameof(Index));
-        }
-
-        private bool CategoryExists(int id)
-        {
-            return (_context.Categories?.Any(e => e.CategoryId == id)).GetValueOrDefault();
         }
     }
 }
