53f3d30376aa7eef9b7e13c866803fd00fe6d624
SteAlb89 <98278002+SteAlb89@users.noreply.github.com>
2023-10-05 09:43:40 +0200

    Create connectionstring and add service dbContext

--- Expenses Tracker/Program.cs
+++ Expenses Tracker/Program.cs
@@ -1,7 +1,14 @@
+using Expenses_Tracker.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+
+
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("MyConnection")));
 
 var app = builder.Build();
 
