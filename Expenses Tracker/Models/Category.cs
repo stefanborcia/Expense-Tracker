22e346592edf1aff1c2165d6f281dabe2015272b
SteAlb89 <98278002+SteAlb89@users.noreply.github.com>
2023-10-06 12:33:55 +0200

    implement validation

--- Expenses Tracker/Models/Category.cs
+++ Expenses Tracker/Models/Category.cs
@@ -8,6 +8,7 @@ namespace Expenses_Tracker.Models
         [Key]
         public int CategoryId { get; set; }
         [Column(TypeName= "nvarchar(50)")]
+        [Required(ErrorMessage = "Title is required.")]
         public string Title { get; set; }
         [Column(TypeName = "nvarchar(5)")]
         public string Icon { get; set; } = "";
