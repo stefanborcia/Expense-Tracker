67f5c62c2690feae8d1030327438a0086837608a
SteAlb89 <98278002+SteAlb89@users.noreply.github.com>
2023-10-06 13:11:11 +0200

    Create a method where we can check the transaction in the last 7 days, total income, total expense, and balance

--- Expenses Tracker/Controllers/DashboardController.cs
+++ Expenses Tracker/Controllers/DashboardController.cs
@@ -1,11 +1,45 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Expenses_Tracker.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Expenses_Tracker.Controllers
 {
     public class DashboardController : Controller
     {
-        public IActionResult Index()
+        private readonly ApplicationDbContext _context;
+        public DashboardController(ApplicationDbContext context)
         {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            //Last 7Days
+            DateTime startDate = DateTime.Today.AddDays(-6);
+            DateTime endDate = DateTime.Today;
+
+            List<Transaction> selectedTransactions = await _context.Transactions
+                .Include(c => c.Category)
+                .Where(d=>d.Date >= startDate && d.Date <= endDate)
+                .ToListAsync();
+
+
+            //Total income
+            int totalIncome = selectedTransactions
+                .Where(t => t.Category.Type == "Income")
+                .Sum(a => a.Amount);
+            ViewBag.totalIncome = totalIncome.ToString("C0"); 
+            
+            //Total expense
+            int totalExpense = selectedTransactions
+                .Where(t => t.Category.Type == "Expense")
+                .Sum(a => a.Amount);
+            ViewBag.totalExpense = totalExpense.ToString("C0");
+
+            //Balance
+            int balance = totalIncome - totalExpense;
+            ViewBag.balance = balance.ToString("C0");
+
             return View();
         }
     }
