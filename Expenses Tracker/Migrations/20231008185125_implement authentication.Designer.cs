f0dd33e9e22be5d363dbdbcbf3bc1e4005a27790
SteAlb89 <98278002+SteAlb89@users.noreply.github.com>
2023-10-08 20:52:30 +0200

    implement user login and user model

--- Expenses Tracker/Migrations/20231008185125_implement authentication.Designer.cs
+++ Expenses Tracker/Migrations/20231008185125_implement authentication.Designer.cs
@@ -4,6 +4,7 @@ using Expenses_Tracker.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Expenses_Tracker.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20231008185125_implement authentication")]
+    partial class implementauthentication
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- Expenses Tracker/Program.cs
+++ Expenses Tracker/Program.cs
@@ -1,4 +1,5 @@
 using Expenses_Tracker.Models;
+using Expenses_Tracker.Repository;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -9,6 +10,7 @@ builder.Services.AddControllersWithViews();
 Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("Ngo9BigBOggjHTQxAR8/V1NHaF5cXmVCf1NpRGFGfV5yd0VOal9XTnRWUiweQnxTdEZiWX9bcHxWQGJVVU13Vw==");
 
 builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("MyConnection")));
+builder.Services.AddScoped<ILogin, AuthenticateLogin>();
 
 var app = builder.Build();
 
