25e4b5b1cc161d08c55f19c6922af1390b12c71b
SteAlb89 <98278002+SteAlb89@users.noreply.github.com>
2023-10-09 12:32:59 +0200

    implement login and register page

--- Expenses Tracker/Models/Transaction.cs
+++ Expenses Tracker/Models/Transaction.cs
@@ -1,7 +1,6 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Globalization;
-using Microsoft.Identity.Client;
 
 namespace Expenses_Tracker.Models
 {
